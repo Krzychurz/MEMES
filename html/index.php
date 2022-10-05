08e163c31f18576254fad38eae06e169c1cb2cc0
Krzychurz <bartekrz66@gmail.com>
2022-10-05 13:56:09 +0200

    kod od gracjana

---
+++ html/index.php
@@ -0,0 +1,42 @@
+<?php
+    $con = new mysqli("localhost", "root", "", "memes");
+    $page = 1;
+    $limit = 10;
+    $offset = ($page * $limit) - $limit;
+    $q ="SELECT `file`, `like`, `created_ad`, `title` FROM memes limit $limit offset $offset ";
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="styl.css">
+        <title>Memy</title>
+    </head>
+    <body>
+             <?php 
+                    if ($wynik = $con->query($q)){
+                        while($row=$wynik->fetch_array()){
+                            //print_r($row);
+                            echo '
+                                <div class="date_title">
+                                    <div class="title"><p>'.$row["title"].'</p></div>
+                                    <div class="date"><p>'.$row["created_ad"].'</p></div>
+                                </div>
+                                <div class="where_meme"></div>
+                                <div class="meme">
+                                <div>
+                                    <img class="meme_1" src="'.$row["file"].'" alt="'.$row["title"].'">
+                                </div>
+                                </div>
+                                    <div class="where_meme">Ilość likeów: '.$row["like"].'</div>
+                                </div>
+                                ';
+                        }
+                    }
+            ?>
+        <div class="pages">
+            <h2>Tu są strony</h2>
+            
+            <button> -- </button> 1 <button> -- </button>
+        </div>
+    </body>
+</html>
